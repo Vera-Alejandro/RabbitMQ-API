0f967f42f8d1369415a0fdc1d388ae2a55eddfb3
Alejandro Vera <Alejandro.vera-gonzalez@trojans.dsu.edu>
2019-12-18 13:53:36 -0600

    Finsihed WinForms App

--- Consumer-Producer/Consumer-Producer.cs
+++ Consumer-Producer/Consumer-Producer.cs
@@ -1,7 +1,11 @@
-﻿using System;
+﻿using Interstates.Control.MessageBus;
+using Interstates.Control.MessageBus.RabbitMq.Implementation;
+using RabbitMQ.Client;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -21,7 +25,27 @@ namespace Consumer_Producer
         private void Download_Click(object sender, EventArgs e)
         {
             string queueName = "Download";
-            
+            string sendingMessage = $"[{DateTimeOffset.Now}]   Hey I want you to download this.";
+
+            ConnectionFactory factory = new ConnectionFactory()
+            {
+                HostName = "localhost",
+                UserName = "alejandro", 
+                Password = "teslamotors"
+            };
+
+            using var connection = factory.CreateConnection();
+            using var channel = connection.CreateModel();
+
+            MessageBusProducerClient producer = new MessageBusProducerClient(channel, queueName);
+
+            var message = new Message<string>(Guid.NewGuid(), sendingMessage);
+
+            producer.PostAsync(message);
+            Debug.WriteLine("Message Sent");
+
+            channel.Close();
+            connection.Close();
         }
     }
 }
